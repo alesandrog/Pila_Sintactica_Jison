3d85d3f9aa4f5fa8b552bc8e81b55e5ae0611cd1
Alesandro Gonzalez <nelsonalesandrog@gmail.com>
2020-08-23 13:46:39 -0600

    Editores

---
+++ js/main.js
@@ -0,0 +1,8 @@
+let input = ace.edit("input_editor");
+let output = ace.edit("output_editor");
+
+
+function analizar (){
+    let res = CalculadoraHeredados.parse(input.getValue());
+    output.setValue(res.toString());
+}
