86f87642e246f702d85f2487f4242948b5e8e340
Alesandro Gonzalez <nelsonalesandrog@gmail.com>
2020-08-23 13:13:50 -0600

    Funcion Analizar

---
+++ js/main.js
@@ -0,0 +1,10 @@
+
+let input = ace.edit("input_editor");
+let output = ace.edit("output_editor");
+
+
+function analizar (){
+    let res = CalculadoraHeredados.parse(input.getValue());
+    output.setValue(res.toString());
+}
+
