c1d2cec43d5d9fb5d91166e20bdde01b524b78a8
Alesandro Gonzalez <nelsonalesandrog@gmail.com>
2020-08-23 13:53:13 -0600

    Boton Limpiar

--- js/main.js
+++ js/main.js
@@ -6,3 +6,8 @@ function analizar (){
     let res = CalculadoraHeredados.parse(input.getValue());
     output.setValue(res.toString());
 }
+
+function limpiar(){
+    input.setValue("");
+    output.setValue("");
+}
